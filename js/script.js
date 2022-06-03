875f7229fb2e0d152e056deb6d528351ef68cc14
step <goianmadalina@gmail.com>
2022-06-03 20:53:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('#slick-caraousel-1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+
+        },
+        {
+            breakpoint: 400,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
